42b6919c428229ad720cf95255d95f138c0eeb54
Vitalii Oliinyk <vitaliyoliynykk@gmail.com>
2021-06-23 14:50:17 +0300

    geocoder

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema(
     {
@@ -103,6 +104,24 @@ const BootcampSchema = new mongoose.Schema(
 
   BootcampSchema.pre('save', function(next) {
     this.slug = slugify(this.name, { lower: true });
+    next();
+  });
+
+  BootcampSchema.pre('save', async function(next) {
+    const location = await geocoder.geocode(this.address);
+    this.location = {
+      type: 'Point',
+      coordinates: [location[0].longitude, location[0].latitude],
+      formattedAddress: location[0].formattedAddress,
+      street: location[0].streetName,
+      city: location[0].city,
+      state: location[0].stateCode,
+      zipcode: location[0].zipcode,
+      country: location[0].countryCode,
+    }
+
+    this.address = undefined;
+
     next();
   })
 
